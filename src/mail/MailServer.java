b47e4afd03513393c3f4ddbdfbfd79b653f8ff0e
“Omer <27omerf@gmail.com>
2024-02-14 11:14:38 +0100

    The test for constructor (for each) line 77 at 98 doesn't work, it will be changed. new file:   class/mail/MailServer.class new file:   class/testMailItem.class new file:   src/mail/MailServer.java new file:   src/testMailItem.java modified:   ws/compiler.bat

---
+++ src/mail/MailServer.java
@@ -0,0 +1,59 @@
+package mail;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MailServer {
+    private ArrayList<MailItem> items;
+
+    public MailServer(){
+        this.items = new ArrayList<MailItem>();
+    }
+
+    public void post(MailItem item){
+        if(items != null){
+            this.items.add(item);
+        } else {
+            System.out.println("MailServer_post : Item Nulle");
+        }
+    }
+
+    /**
+     * @param who
+     * @return
+     */
+    public int howManyMailItems(String who){
+        int compteur = 0;
+        if(who == null){
+            System.out.println("MailServer_howManyMailItems : Valeur Nulle");
+        } else {
+            for (MailItem item : items) {
+                if(((item.getTo())).equals(who)){
+                    compteur++;
+                }
+            }
+        }
+        return(compteur);
+    }
+
+    public MailItem getNextMailItem(String who){
+        MailItem prochainMail = null;
+
+        if(who == null){
+            System.out.println("MailServer_getNextMailItem : Valeur Nukke");
+        } else {
+            if(howManyMailItems(who) > 0){
+                boolean found = false;
+                Iterator<MailItem> it = this.items.iterator();
+                while(it.hasNext() || !found){
+                    MailItem nextItem = it.next();
+                    if(nextItem.getTo().equals(who)){
+                        it.remove();
+                        found = true;
+                        prochainMail = nextItem;
+                    }
+                }
+            }
+        }
+        return(prochainMail);
+    }
+}
--- ws/compiler.bat
+++ ws/compiler.bat
@@ -1,3 +1,7 @@
 javac -d ../class ../src/mail/MailItem.java
+javac -d ../class ../src/mail/MailServer.java
+javac -d ../class ../src/testMailItem.java
+
+java testMailItem
 
 pause
